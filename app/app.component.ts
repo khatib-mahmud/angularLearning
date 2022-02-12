f3aecfa62dbce038b6aa60ca4da6a76610e3fb31
khatibmahmud49@gmail.com <khatibmahmud49@gmail.com>
2022-02-12 23:11:14 +0600

    first

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('', 'rob@test.com', 12431343, '', 'morning', true)
+
+}
